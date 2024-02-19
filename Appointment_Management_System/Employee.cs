0545b6dc15227bdf08469a9a10732270944e073d
Shreejal Maharjan <shreejal27@gmail.com>
2024-02-19 23:20:32 +0545

    added requirements to implement

---
+++ Appointment_Management_System/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_Management_System
+{
+    internal class Employee
+    {
+        private int employeeId;
+        private string employeeName = string.Empty;
+        private int emp_Age;
+
+
+        public int EmployeeId
+        {
+            get;
+            set;
+        }
+        public string EmployeeName
+        {
+            get
+            {
+                return employeeName; //return the value
+            }
+            set  //assigns the value
+            {
+                employeeName = value.Length > 50 ? value[..50]: value; //custom logic can be included here
+            }
+        }
+    }
+}
--- Appointment_Management_System/Program.cs
+++ Appointment_Management_System/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+ Requirements of projects
+
+    List of employees working in a company.
+    Employees : start and end time, position.
+
+   Problem Statement: Book an appointment
+
+   How System will work: 
+            Client wants to talk to a specific position of the company
+            Check if there is the position in system, if yes list number of employees in the position, 
+            Ask user to input time of appointment, take input time and check if slots are available within that time period,
+                if no slots suggest the nearest time to user
+                if open slots book the appointment with the employee
+            Send the booked date via email to both employee and client
+
+    Employee Class:
+            Id
+            Name
+            Position
+            Working Time
+            
+ */
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is Appointment Management System");
